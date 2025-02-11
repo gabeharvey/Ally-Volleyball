16eddacea06d7be4ec083af6f67dde76e117936a
Gabe Harvey <harvey7@gmail.com>
2025-02-10 21:48:14 -0600

    Updated Video 2 on VolleyballFilm.jsx

--- src/components/VolleyballFilm.jsx
+++ src/components/VolleyballFilm.jsx
@@ -51,7 +51,7 @@ function VolleyballFilm() {
           description: "Laying Everything on the Line",
         },
         {
-          videoSrc: "ally-vid-3.mov",
+          videoSrc: "ally-serve.mp4",
           title: "Excellent Serve",
           description: "Pinpoint Accuracy on Serve",
         },
